256a1c9577b85f58e8548e6ad63288f2a570e127
KaremMetrial <karim.metrial@gmail.com>
2025-04-02 03:43:04 +0200

    contact page

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('web.index');
 });
+
+Route::get('/contact', function() {
+    return view('web.contact-us');
+});
